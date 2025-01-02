43aa4133f709d997acc75a15ce2f69593753a3c2
Andre Ricardo <andre.ricardo@zoetis.com>
2025-01-02 15:20:24 +0000

    Changes run locally in Docker.
    Add build and valgrind tasks.
    
    Fix leak in steps_handler.cpp IsInstalled
    
    Details
    ```
    ==1917582== 32 bytes in 2 blocks are definitely lost in loss record 36 of 75
    ==1917582==    at 0x4838DEF: operator new(unsigned long) (vg_replace_malloc.c:342)
    ==1917582==    by 0x8817A64: ScriptHandlerImpl::CreateContentHandler() (script_handler.cpp:117)
    ==1917582==    by 0x88178E1: CreateUpdateContentHandlerExtension (script_handler.cpp:55)
    ==1917582==    by 0x8762261: ExtensionManager::LoadUpdateContentHandlerExtension(std::__cxx11::basic_string<char, std::char_traits<char>, std::allocator<char> > const&, ContentHandler**) (extension_manager.cpp:286)
    ==1917582==    by 0x876025B: StepsHandler_IsInstalled(tagADUC_WorkflowData const*) (steps_handler.cpp:1406)
    ==1917582==    by 0x87605B1: StepsHandlerImpl::IsInstalled(tagADUC_WorkflowData const*) (steps_handler.cpp:1502)
    ==1917582==    by 0x8760341: StepsHandler_IsInstalled(tagADUC_WorkflowData const*) (steps_handler.cpp:1419)
    ==1917582==    by 0x87605B1: StepsHandlerImpl::IsInstalled(tagADUC_WorkflowData const*) (steps_handler.cpp:1502)
    ==1917582==    by 0x18CA63: ADUC::LinuxPlatformLayer::IsInstalled(tagADUC_WorkflowData const*) (linux_adu_core_impl.cpp:329)
    ==1917582==    by 0x18E19B: ADUC::LinuxPlatformLayer::IsInstalledCallback(void*, void*)::{lambda()#1}::operator()() const (linux_adu_core_impl.hpp:334)
    ==1917582==    by 0x18F5A3: tagADUC_Result ADUC::ExceptionUtils::CallResultMethodAndHandleExceptions<ADUC::LinuxPlatformLayer::IsInstalledCallback(void*, void*)::{lambda()#1}>(int, ADUC::LinuxPlatformLayer::IsInstalledCallback(void*, void*)::{lambda()#1} const&) (exception_utils.hpp:60)
    ==1917582==    by 0x18E207: ADUC::LinuxPlatformLayer::IsInstalledCallback(void*, void*) (linux_adu_core_impl.hpp:335)
    ```
    
    Add task to kill AducIotAgent
    
    Revert "Fix leak in steps_handler.cpp IsInstalled"
    
    This reverts commit 9be443a1c4fa7bd7158660fc3f405d274b678ccf.
    
    Change log for extension register step.
    
    Add comment on how to debug forked process.
    
    Fix build in Docker due to changes in scripts/build.sh
    
    Properly initialize curl content handler logging
    
    Pass logLevel to contentDownloader.
    
    Fix do-client logging initialization.
    
    Call Cleanup when the lib is being Unloaded.
    
    Clean up and move Zoetis specific changes to postCreateCommand.sh
    
    Remove mentions of Zoetis.
    
    Reintegrate changes from develop.
    
    Clean up.
    
    Clean up.
    
    Enable test integration.

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,111 @@
+#!/bin/bash
+set -e
+
+mkdir -p /etc/adu
+cp scripts/docker/templates/du-config.template.json /etc/adu/du-config.json
+cp scripts/docker/templates/du-diagnostics-config.template.json /etc/adu/du-diagnostics-config.json
+
+
+DUCONFIG_COMPAT_PROPERTY_NAMES="manufacturer,model"
+DUCONFIG_DEVICEINFO_MANUFACTURER="your_company_name"
+DUCONFIG_DEVICEINFO_MODEL="Hub"
+DUCONFIG_AGENT_NAME="your_company_name-docker-agent"
+DUCONFIG_CONNECTION_STRING=${ADU_CONNECTION_STRING} # from .devcontainer/devcontainer.env
+DUCONFIG_DEVICEPROPERTIES_MANUFACTURER="your_company_name"
+DUCONFIG_DEVICEPROPERTIES_MODEL="Hub"
+
+inline_expand_template_parameters() {
+    params_to_replace="$1"
+    output_file_path="$2"
+
+    for var in $params_to_replace; do
+        pattern="%%${var}%%"
+        param_val=${!var}
+        param_val=${param_val/\./\\\.} # escape periods
+        param_val=${param_val/\&/\\\&} # escape ampersands
+
+        sed -i "s|${pattern}|${param_val}|g" "$output_file_path"
+    done
+}
+
+## ADU connection string
+parameters_to_expand="
+    DUCONFIG_COMPAT_PROPERTY_NAMES
+    DUCONFIG_DEVICEINFO_MANUFACTURER
+    DUCONFIG_DEVICEINFO_MODEL
+    DUCONFIG_AGENT_NAME
+    DUCONFIG_CONNECTION_STRING
+    DUCONFIG_DEVICEPROPERTIES_MANUFACTURER
+    DUCONFIG_DEVICEPROPERTIES_MODEL
+"
+inline_expand_template_parameters "$parameters_to_expand" "/etc/adu/du-config.json"
+
+# Build the ADU
+scripts/build.sh -c --work-folder .workspace
+
+# Make setup_container.sh happy that these files are present, since we don't actually need them to run the container.
+cp out/bin/adu-shell /usr/bin/adu-shell
+cp out/bin/AducIotAgent /usr/bin/AducIotAgent
+
+# Microsoft script to setup container in general
+scripts/docker/templates/setup_container.sh
+
+cp out/lib/libcurl_content_downloader.so /var/lib/adu/extensions/sources/
+cp out/lib/libmicrosoft_script_1.so /var/lib/adu/extensions/sources/
+cp out/lib/libmicrosoft_steps_1.so /var/lib/adu/extensions/sources/
+cp out/lib/libcontoso_component_enumerator.so /var/lib/adu/extensions/sources/
+
+## configure content downloader extension
+so_name="libcurl_content_downloader"
+base64digest=$(openssl dgst -binary "/var/lib/adu/extensions/sources/libcurl_content_downloader.so" | openssl base64)
+
+parameters_to_expand="
+    so_name
+    base64digest
+"
+target_filepath="/var/lib/adu/extensions/content_downloader/extension.json"
+
+mkdir -p "$(dirname "$target_filepath")"
+cp "scripts/docker/templates/content_downloader.extension.template.json" "$target_filepath"
+inline_expand_template_parameters "$parameters_to_expand" "$target_filepath"
+
+# recreate some of the steps from packages/debian/postinst in particular function "register_reference_extensions"
+
+#adu_bin_path=out/bin/AducIotAgent
+#$adu_extensions_sources_dir/$adu_steps_handler_file
+
+out/bin/AducIotAgent -l 0 --extension-type updateContentHandler --extension-id "microsoft/script:1" --register-extension /var/lib/adu/extensions/sources/libmicrosoft_script_1.so
+out/bin/AducIotAgent -l 0 --extension-type updateContentHandler --extension-id "microsoft/steps:1" --register-extension /var/lib/adu/extensions/sources/libmicrosoft_steps_1.so
+out/bin/AducIotAgent -l 0 --extension-type updateContentHandler --extension-id "microsoft/update-manifest:5" --register-extension /var/lib/adu/extensions/sources/libmicrosoft_steps_1.so
+
+out/bin/AducIotAgent -l 0 --extension-type componentEnumerator --register-extension /var/lib/adu/extensions/sources/libcontoso_component_enumerator.so
+
+out/bin/AducIotAgent -l 0 --extension-type contentDownloader --register-extension /var/lib/adu/extensions/sources/libcurl_content_downloader.so
+
+mkdir -p /usr/local/contoso-devices/
+
+sample_components_inventory=$(
+    cat << END_OF_JSON
+{
+    "components": [
+        {
+            "id": "vetscan-hub",
+            "name": "vetscan-hub",
+            "group": "Hub",
+            "manufacturer": "your_company_name",
+            "model": "Hub",
+            "status": "available",
+            "hwVersion": "",
+            "swVersion": "1.7.3",
+            "lasttriedversion": "1.5.0",
+            "properties": {
+                "path": "/usr/local/contoso-devices/",
+                "firmwareDataFile": "components-inventory.json"
+            }
+        }
+    ]
+}
+END_OF_JSON
+)
+
+echo "$sample_components_inventory" > "/usr/local/contoso-devices/components-inventory.json"
